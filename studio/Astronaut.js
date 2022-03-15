ab2c655e2c68331379f8d214346460f98a1c3ee6
billbass71 <billminton71@gmail.com>
2022-03-15 11:23:23 -0500

    finished studio

---
+++ studio/Astronaut.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Astronaut = void 0;
+var Astronaut = /** @class */ (function () {
+    // properties and methods
+    function Astronaut(massKg, name) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+    return Astronaut;
+}());
+exports.Astronaut = Astronaut;
